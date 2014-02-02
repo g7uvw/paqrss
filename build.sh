1e7a90e3222b445322e0700899f0116122f42635
David on the rPI <dave-rpi@webshed.org>
2014-02-02 21:53:46 +0000

    Working on PI now, takes about 10% CPU. Pulse Audio doesn't seem to do on-fly resampling, like I thought it did

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 clang -DPACKAGE_NAME=\"paqrss\" -DPACKAGE_TARNAME=\"paqrss\" -DPACKAGE_VERSION=\"0.01\" -DPACKAGE_STRING=\"paqrss\ 0.01\" -DPACKAGE_BUGREPORT=\"\" -DPACKAGE_URL=\"\" -DPACKAGE=\"paqrss\" -DVERSION=\"0.01\" -I. -Wall -g -O2 -MT paqrss.o -MD -MP -MF .deps/paqrss.Tpo -c -o paqrss.o paqrss.c
 mv -f .deps/paqrss.Tpo .deps/paqrss.Po
-clang -Wall -g -O2   -o paqrss paqrss.o  -lfftw3 -lm  -lpulse -lpulse-simple
+clang -Wall -g -O2   -o paqrss paqrss.o  -lfftw3 -lm  -lfreeimage -lpulse -lpulse-simple
 
